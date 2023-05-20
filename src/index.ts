252ed0c28699c3b3a900d8fb9e9cae31edf709ce
Firas-Alkhatib <firas.alkhatib@solita.fi>
2023-05-20 04:08:23 +0300

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
+import cors from 'cors'
 import { router } from 'routes'
 
 const PORT = process.env.PORT || '3000'
+const corsMiddleware = cors({
+  origin: process.env.FRONTEND_ORIGIN,
+})
 const app = express()
-
+app.use(corsMiddleware)
 app.use(express.json())
 
 app.use(router)
