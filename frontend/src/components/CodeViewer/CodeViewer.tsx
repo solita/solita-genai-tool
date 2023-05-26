43a289dab35b61733d8936cd3241f14a2122b358
solita-jajuhir <jaakko.hirvioja@solita.fi>
2023-05-26 15:05:04 +0300

    Dark mode, transformed css to be more scoped

--- frontend/src/components/CodeViewer/CodeViewer.tsx
+++ frontend/src/components/CodeViewer/CodeViewer.tsx
@@ -11,19 +11,25 @@ const MIN_HEIGHT = 100
 
 export const CodeViewer = ({ children, language }: IProps) => {
   return (
-    <div className={styles.codeViewer}>
-      <Editor
-        language={language}
-        options={{ readOnly: true, minimap: { enabled: false } }}
-        theme="vs-dark"
-        defaultValue={children}
-        onMount={(editor) => {
-          const lineCount = editor.getModel()?.getLineCount() || 0
-          const height = Math.min(Math.max(lineCount * LINE_HEIGHT, MIN_HEIGHT), MAX_HEIGHT)
-          editor.getContainerDomNode().style.height = height + 'px'
-          editor.layout()
-        }}
-      />
-    </div>
+    <>
+      <div className={styles.container}>
+        {language ? <div className={styles.language}>{language}</div> : ''}
+        <div className={styles.codeViewer}>
+          <Editor
+            language={language}
+            options={{ readOnly: true, minimap: { enabled: false }, automaticLayout: true }}
+            theme="vs-dark"
+            defaultValue={children}
+            className={styles.code}
+            onMount={(editor) => {
+              const lineCount = editor.getModel()?.getLineCount() || 0
+              const height = Math.min(Math.max(lineCount * LINE_HEIGHT, MIN_HEIGHT), MAX_HEIGHT)
+              editor.getContainerDomNode().style.height = height + 'px'
+              editor.layout()
+            }}
+          />
+        </div>
+      </div>
+    </>
   )
 }
